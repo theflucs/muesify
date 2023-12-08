a83d62e829b5b154f64ecd97be9db61e44340065
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 17:02:53 +0100

    implement getUserProfile api

--- src/api/services.js
+++ src/api/services.js
@@ -1,1 +1,10 @@
 import { http } from "@/api/axios";
+export const getUserProfile = async () => {
+    try {
+        const response = await http.get("me");
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
