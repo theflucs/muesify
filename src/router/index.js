c8d207cf5f9df2daa2447699c1e2cc8d4940901c
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 16:11:45 +0100

    feat: add navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import AuthView from "@/views/AuthView.vue";
 import Unauthorized from "@/views/Unauthorized.vue";
 
+const isAuthenticated = () => {
+    const accessToken = localStorage.getItem("access_token");
+    return accessToken !== null;
+};
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -9,23 +14,36 @@ const router = createRouter({
             path: "/unauthorized",
             name: "unauthorized",
             component: Unauthorized,
+            meta: { requiresAuth: false },
         },
         {
             path: "/",
             name: "auth",
             component: AuthView,
+            meta: { requiresAuth: false },
         },
         {
             path: "/home",
             name: "home",
             component: () => import("@/views/HomeView.vue"),
+            meta: { requiresAuth: true },
         },
         {
             path: "/about",
             name: "about",
             component: () => import("@/views/AboutView.vue"),
+            meta: { requiresAuth: true },
         },
     ],
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !isAuthenticated()) {
+        console.log("redirect to unauth from router");
+        next("/unauthorized");
+    } else {
+        next();
+    }
 });
 
 export default router;
