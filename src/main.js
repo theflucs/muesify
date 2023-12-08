974e6e6b99a1fc19bdea323cdde219c3cbc77633
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 16:58:24 +0100

    add vuex

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import store from "@/store";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 app.use(VueAxios, axios);
 app.mount("#app");
