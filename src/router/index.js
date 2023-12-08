0873e7cb1054b691f0d6b1f150b199d21c94acb2
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 16:11:16 +0100

    create unauthorized view

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AuthView from "@/views/AuthView.vue";
+import Unauthorized from "@/views/Unauthorized.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: "/unauthorized",
+            name: "unauthorized",
+            component: Unauthorized,
+        },
         {
             path: "/",
             name: "auth",
