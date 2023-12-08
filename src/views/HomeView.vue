561fee23e44b46ea624b000cbde644db4214e767
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 17:06:08 +0100

    feat: use of apis to get user profile and playlists

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,49 @@
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
+import { useStore } from 'vuex';
+import { getUserProfile, getUserPlaylists } from '@/api/services.js'
+
 export default {
   name: 'home',
   components: {
 
   },
   setup() {
+    const store = useStore()
+    const profile = ref(null);
+
+    const isAuthenticated = () => {
+      const accessToken = localStorage.getItem("access_token");
+      return accessToken !== null;
+    };
+
+    onMounted(async () => {
+      if (isAuthenticated()) {
+        const res = await getProfile();
+      }
+    })
+
+    const getProfile = (async () => {
+      try {
+        profile.value = await getUserProfile();
+        store.dispatch("setUser", profile.value);
+        await getPlaylists();
+      } catch (error) {
+        console.error('Authentication error:', error);
+      }
+    })
+
+    const getPlaylists = (async () => {
+      try {
+        const userId = store.state.user.id;
+        const res = await getUserPlaylists(userId);
+        console.log('playlists', res.items)
+      } catch (error) {
+        console.error('Failed getting UserPlaylists', error);
+      }
+    })
   }
 }
 </script>
