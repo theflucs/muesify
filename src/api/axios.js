0430b06ce046fc5755503790b9f5939398b38a1d
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 22:30:07 +0100

    fix axios interceptors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { SPOTIFY_BASE_API_URL } from "@/api/endpoints";
+import Unauthorized from "@/views/Unauthorized.vue";
 
 export const http = axios.create({
     baseURL: SPOTIFY_BASE_API_URL,
@@ -16,20 +17,40 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error && error.status === 401) {
-            const data = {
-                grant_type: refresh_token,
-                refresh_token: localStorage.getItem(
-                    "refresh_token",
-                    refresh_token
-                ),
-                client_id: `${import.meta.env.VITE_CLIENT_ID}`,
-            };
-            return Promise.reject({ component: UnauthorizedView });
-        } else {
-            console.log("An error occurred. Please try again later.");
-            return Promise.reject(error);
-        }
+        async (error) => {
+            if (error && error.status === 401) {
+                const data = {
+                    grant_type: "refresh_token",
+                    refresh_token: localStorage.getItem("refresh_token"),
+                    client_id: `${import.meta.env.VITE_CLIENT_ID}`,
+                };
+                const body = new URLSearchParams(data);
+                try {
+                    const res = await axios.post(
+                        "https://accounts.spotify.com/api/token",
+                        body
+                    );
+                    const { access_token, token_type, refresh_token } =
+                        res.data;
+                    localStorage.setItem("access_token", access_token);
+                    localStorage.setItem("token_type", token_type);
+                    localStorage.setItem("refresh_token", refresh_token);
+                    // error.response.config.headers.Authorization = `Bearer ${access_token}`
+                    return axios({
+                        ...error.response.config,
+                        headers: {
+                            ...error.response.config.headers,
+                            Authorization: `Bearer ${access_token}`,
+                        },
+                    });
+                } catch (error) {
+                    return Promise.reject(error, { component: Unauthorized });
+                }
+            } else {
+                console.log("An error occurred. Please try again later.");
+                return Promise.reject(error, { component: Unauthorized });
+            }
+        };
     }
 );
 
@@ -60,7 +81,7 @@ http.interceptors.response.use(
                 });
             } catch (error) {
                 localStorage.clear();
-                return Promise.reject(error);
+                return Promise.reject(error, { component: Unauthorized });
             }
         } else {
             console.log("An error occurred. Please try again later.");
