de1ec1f8fcc441b4faff373ee8e0d59cb7115dec
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 15:00:27 +0100

    feat: implement auth - create auth view - update routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AuthView from "@/views/AuthView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
+            name: "auth",
+            component: AuthView,
+        },
+        {
+            path: "/home",
             name: "home",
-            component: HomeView,
+            component: () => import("@/views/HomeView.vue"),
         },
         {
             path: "/about",
             name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/AboutView.vue"),
+            component: () => import("@/views/AboutView.vue"),
         },
     ],
 });
