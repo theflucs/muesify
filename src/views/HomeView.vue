8582078131e7ee088af7730aebfaa84ae32a955b
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 19:03:11 +0100

    WIP:  app structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
-    <h1>Home</h1>
+    <section id="home-section">
+      <h1>Home</h1>
+      <List type="featuredPlaylists" />
+      <List type="playlists" v-if="profile" />
+    </section>
   </main>
 </template>
 
@@ -9,11 +13,12 @@ import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex';
 import { getUserProfile } from '@/api/services'
 import { isAuthenticated } from "@/utils";
+import List from '@/views/List.vue'
 
 export default {
   name: 'home',
   components: {
-
+    List
   },
   setup() {
     const store = useStore()
