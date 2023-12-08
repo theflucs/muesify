590bee44c1430987bed30752da8305259b142e66
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 16:11:16 +0100

    create unauthorized view

---
+++ src/views/Unauthorized.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>401 You are not authorized</h1>
+  <p>You don't have permission to access this page.</p>
+  <router-link class="btn btn-primary" to="/">Authenticate</router-link>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'unauthorized',
+  components: {
+
+  },
+  setup() {
+  }
+}
+</script>
+<style scoped></style>
