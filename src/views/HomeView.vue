221316a2bd8970c6fa8d7f46a4d75b9cf79acc32
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-08 13:54:42 +0100

    cleanup app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
   </main>
 </template>
+
+<script>
+export default {
+  name: '',
+  components: {
+
+  },
+  setup() {
+  }
+}
+</script>
+
+<style scoped></style>
