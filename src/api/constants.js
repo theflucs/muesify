d7cdfc2023ee2a2956ee3af3adf9fa66af0dda53
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-09 11:45:54 +0100

    add api scope

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
 export const SCOPE =
-    "playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-email user-read-private";
+    "playlist-read-private, playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-email user-read-private";
 export const CHALLENGE_METHOD = "S256";
 export const GRANT_TYPE = "authorization_code";
