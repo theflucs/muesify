9cb7851282b6895f08a84d7eb3f34acf02ed073f
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-09 11:45:13 +0100

    add global utils

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,13 @@ export function isAuthenticated() {
     const accessToken = localStorage.getItem("access_token");
     return accessToken !== null;
 }
+export function formatString(string) {
+    return (
+        string.charAt(0).toUpperCase() +
+        string.slice(1).replace(/[A-Z]/g, (match) => ` ${match}`)
+    );
+}
+export const removeHtmlTags = (string) => {
+    const tagIndex = string.indexOf("<");
+    return tagIndex !== -1 ? string.substring(0, tagIndex) : string;
+};
