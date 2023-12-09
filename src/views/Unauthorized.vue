640a3cdd1ac0b1b716860ff56bb55e25235679f3
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-09 11:56:28 +0100

    style: Unauthorized view

--- src/views/Unauthorized.vue
+++ src/views/Unauthorized.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1>401 You are not authorized</h1>
-  <p>You don't have permission to access this page.</p>
-  <router-link class="btn btn-primary" to="/">Authenticate</router-link>
+  <section id="authorize-or-login" class="row d-flex text-center align-items-center vh-100">
+    <div class="col-md-6 mx-auto">
+      <h1 class="mue-yellow">401</h1>
+      <h3 class="my-5">Sorry, you are not authorized to access this page.</h3>
+      <Router-link role="buton" class="btn mue-btn-yellow pill" to="/">Authorize in App</Router-link>
+    </div>
+  </section>
 </template>
 
-<script lang="ts">
+<script>
 
 export default {
   name: 'unauthorized',
