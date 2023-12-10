8b2d481951265f744b28d5fb62c0b40b79cff80e
sa.cux <sabrina.cuccureddu@gmail.com>
2023-12-10 19:47:57 +0100

    fix: axios request interceptor

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import router from "@/router";
-import { SPOTIFY_BASE_API_URL } from "@/api/endpoints";
-import Unauthorized from "@/views/Unauthorized.vue";
+import { SPOTIFY_BASE_API_URL, GET_TOKEN_URL } from "@/api/endpoints";
 
 export const http = axios.create({
     baseURL: SPOTIFY_BASE_API_URL,
 });
 
-http.interceptors.request.use((config) => {
+http.interceptors.request.use(async (config) => {
     const token = localStorage.getItem("access_token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -18,75 +17,46 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use(
     (response) => response,
     async (error) => {
-        if (error && error.status === 401) {
-            const data = {
-                grant_type: "refresh_token",
-                refresh_token: localStorage.getItem("refresh_token"),
-                client_id: `${import.meta.env.VITE_CLIENT_ID}`,
-            };
-            const body = new URLSearchParams(data);
-            try {
-                const res = await axios.post(
-                    "https://accounts.spotify.com/api/token",
-                    body
+        if (error.response && error.response.status === 401) {
+            console.log("Unauthorized");
+            if (localStorage.getItem("refresh_token")) {
+                console.log(
+                    "we have refresh token in localstorgae, try to refresh it"
                 );
-                const { access_token, token_type, refresh_token } = res.data;
-                localStorage.setItem("access_token", access_token);
-                localStorage.setItem("token_type", token_type);
-                localStorage.setItem("refresh_token", refresh_token);
-                // error.response.config.headers.Authorization = `Bearer ${access_token}`
-                return axios({
-                    ...error.response.config,
-                    headers: {
-                        ...error.response.config.headers,
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                });
-            } catch (error) {
-                localStorage.clear();
-                router.push("/unauthorized");
-                return Promise.reject(error);
-            }
-        } else {
-            console.log("Error 401 Unauthorized");
-            router.push("/unauthorized");
-            return Promise.reject(error);
-        }
-    }
-);
+                const data = {
+                    grant_type: "refresh_token",
+                    refresh_token: localStorage.getItem("refresh_token"),
+                    client_id: `${import.meta.env.VITE_CLIENT_ID}`,
+                };
+                const body = new URLSearchParams(data);
+                try {
+                    console.log("Trying to get refresh token");
+                    const res = await axios.post(GET_TOKEN_URL, body);
+                    const { access_token, token_type, refresh_token } =
+                        res.data;
+                    localStorage.setItem("access_token", access_token);
+                    localStorage.setItem("token_type", token_type);
+                    localStorage.setItem("refresh_token", refresh_token);
 
-// refresh token
-http.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        if (error && error.status === 401) {
-            const data = {
-                grant_type: "refresh_token",
-                refresh_token: localStorage.getItem("refresh_token"),
-                client_id: `${import.meta.env.VITE_CLIENT_ID}`,
-            };
-            const body = new URLSearchParams(data);
-            try {
-                const res = await axios.post(GET_TOKEN_URL, body);
-                const { access_token, token_type, refresh_token } = res.data;
-                localStorage.setItem("access_token", access_token);
-                localStorage.setItem("token_type", token_type);
-                localStorage.setItem("refresh_token", refresh_token);
-                // error.response.config.headers.Authorization = `Bearer ${access_token}`
-                return axios({
-                    ...error.response.config,
-                    headers: {
-                        ...error.response.config.headers,
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                });
-            } catch (error) {
-                localStorage.clear();
-                console.log("Error trying to refresh token");
-                router.push("/unauthorized");
+                    // Retry the original request with the new access token
+                    error.config.headers.Authorization = `Bearer ${access_token}`;
+                    return http(error.config);
+                } catch (error) {
+                    // Clear tokens and navigate to unauthorized page
+                    localStorage.clear();
+                    console.log("Error trying to refresh token");
+                    router.push("/unauthorized");
+                    return Promise.reject(error);
+                }
+            } else {
+                console.log("no toke, start the auth flow");
+                router.push("/");
                 return Promise.reject(error);
             }
         } else {
+            // For other errors, just reject the promise
+            console.log("Oops! Something went wrong.");
+            console.log("here");
             return Promise.reject(error);
         }
     }
